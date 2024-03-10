d03becbe05ed719cf39928f2b2d4a19791a510d6
Isra Imran <161215461+IsraImran00@users.noreply.github.com>
2024-03-10 19:21:21 +0500

    Add files via upload

---
+++ Q9.js
@@ -0,0 +1,4 @@
+/* Favourite Number: Store your favourite number in a variable. Then, using that variable
+ create a message that reveals your favourite number. Print that message.*/
+var favNum = 3011;
+console.log("My Favourite Number is ".concat(favNum));
